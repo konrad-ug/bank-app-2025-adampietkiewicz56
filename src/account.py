947032dce303989e8e3d752b8e394b4f81ab26db
Adam Pietkiewicz <apietkiewicz@INF.UG.EDU.PL>
2025-10-16 13:28:58 +0200

    feature 3

--- src/account.py
+++ src/account.py
@@ -1,7 +1,7 @@
 class Account:
     def __init__(self, first_name, last_name, pesel, balance = 0):
+
         self.first_name = first_name
         self.last_name = last_name
-        self.pesel = pesel
+        self.pesel = pesel if len(pesel) == 11 else "Invalid"
         self.balance = balance
-
--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -1,6 +1,5 @@
 from src.account import Account
 
-
 class TestAccount:
     def test_account_creation(self):
         account = Account("John", "Doe", "02040722492", 0)
@@ -8,3 +7,15 @@ class TestAccount:
         assert account.last_name == "Doe"
         assert account.pesel == "02040722492"
         assert account.balance == 0
+
+    def test_valid_pesel(self):  
+        acc = Account("Anna", "Nowak", "90010112345")
+        assert acc.pesel == "90010112345"
+
+    def test_invalid_pesel_short(self): 
+        acc = Account("Piotr", "Kowalski", "1234567890")
+        assert acc.pesel == "Invalid"
+
+    def test_invalid_pesel_long(self):
+        acc = Account("Ala", "Makota", "123456789012")
+        assert acc.pesel == "Invalid"
