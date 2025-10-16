e932ddc586bcfbe327981114053c967403d3ad1c
Adam Pietkiewicz <apietkiewicz@inf.ug.edu.pl>
2025-10-16 13:13:08 +0200

    feature 2

--- tests/unit/test_account_create.py
+++ tests/unit/test_account_create.py
@@ -3,7 +3,8 @@ from src.account import Account
 
 class TestAccount:
     def test_account_creation(self):
-        account = Account("John", "Doe", 0)
+        account = Account("John", "Doe", "02040722492", 0)
         assert account.first_name == "John"
         assert account.last_name == "Doe"
+        assert account.pesel == "02040722492"
         assert account.balance == 0
