54326815881c1232c706d8cca061cdd640d997f0
Adam Pietkiewicz <apietkiewicz@inf.ug.edu.pl>
2025-10-23 12:39:43 +0200

    koniec lab2

--- src/account.py
+++ src/account.py
@@ -1,7 +1,11 @@
 class Account:
-    def __init__(self, first_name, last_name, pesel, balance = 0):
+    def __init__(self, first_name, last_name, pesel, promo_code = None, balance = 0):
 
         self.first_name = first_name
         self.last_name = last_name
         self.pesel = pesel if len(pesel) == 11 else "Invalid"
         self.balance = balance
+
+        if promo_code and promo_code.startswith("PROM_"):
+            self.balance += 50
+
