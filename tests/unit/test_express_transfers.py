4f9288e918200aef17355aead77ba9ae446f0d1b
Adam Pietkiewicz <apietkiewicz@INF.UG.EDU.PL>
2025-10-30 13:19:07 +0100

    koniec lab2

--- tests/unit/test_express_transfers.py
+++ tests/unit/test_express_transfers.py
@@ -4,14 +4,14 @@ from src.company_account import CompanyAccount
 
 class TestExpressTransfers:
 
-    def test_express_personal_transfer(self):
+    def express_outgoing_pers(self):
         account = PersonalAccount("Alice", "Johnson", "12345678901")
         account.balance = 100.0
 
         account.send_personal_express_transfer(50.0)
         assert account.balance == 49.00
 
-    def test_express_company_transfer(self):
+    def express_outgoing_comp(self):
         account = CompanyAccount("Firma", "1234567890")
         account.balance = 100.0
         account.send_company_express_transfer(50.0)
