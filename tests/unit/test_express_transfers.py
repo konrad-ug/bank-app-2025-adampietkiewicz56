59cb6f2c213dddeac70253997492f624033a8855
Adam Pietkiewicz <apietkiewicz@INF.UG.EDU.PL>
2025-10-30 13:54:49 +0100

    lab3 w trakcie

--- tests/unit/test_express_transfers.py
+++ tests/unit/test_express_transfers.py
@@ -4,15 +4,26 @@ from src.company_account import CompanyAccount
 
 class TestExpressTransfers:
 
-    def express_outgoing_pers(self):
+    def test_express_outgoing_pers(self):
         account = PersonalAccount("Alice", "Johnson", "12345678901")
         account.balance = 100.0
+        account.express_outgoing_pers(50.0)
+        assert account.balance == 49.0
 
-        account.send_personal_express_transfer(50.0)
-        assert account.balance == 49.00
-
-    def express_outgoing_comp(self):
+    def test_express_outgoing_comp(self):
         account = CompanyAccount("Firma", "1234567890")
         account.balance = 100.0
-        account.send_company_express_transfer(50.0)
-        assert account.balance == 45.00
+        account.express_outgoing_comp(50.0)
+        assert account.balance == 45.0
+    
+    def test_express_outgoing_comp_negative_amount(self):
+        account = CompanyAccount("Firma", "1234567890")
+        account.balance = 100.0
+        account.express_outgoing_comp(-10.0)
+        assert account.balance == 100.0  # brak zmiany
+
+    def test_express_outgoing_comp_insufficient_funds(self):
+        account = CompanyAccount("Firma", "1234567890")
+        account.balance = 40.0
+        account.express_outgoing_comp(50.0)
+        assert account.balance == 40.0  # brak zmiany
